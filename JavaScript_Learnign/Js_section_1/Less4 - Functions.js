6b19d4cb7e048b00c7c278975ce17c578449574d
Andrea D'Aguanno <daguannoandrea@outlook.com>
2021-06-19 13:03:12 +0200

    clockworks

--- JavaScript_Learnign/Js_section_1/Less4 - Functions.js
+++ JavaScript_Learnign/Js_section_1/Less4 - Functions.js
@@ -31,3 +31,26 @@ function countDown(int) {
 }
 
 countDown(10)
+
+
+/* Sintassi per dichiarare e richiamre la funzione senza dover riscrivere il nome */
+(function detectChanges() {
+    console.log("Detect Changes");
+})();
+
+
+
+/** Rest operator
+ * Passiamo quanti argoemnti vogliamo e finiranno in una sorta di array */
+function buttonManager(num1, num2, ...nums) {
+    console.log(nums);
+    let sum = 0;
+    for (let i = 0; i < nums; i++) {
+        sum += nums[i];
+    }
+    console.log(sum);
+    return sum;
+
+}
+
+buttonManager(1, 2, 3, 4, 5, 6, 7, 8, 9)
