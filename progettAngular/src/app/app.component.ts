8a6dcbb9f3319291b79544708d4f577301fe8f3c
Andrea D'Aguanno <daguannoandrea@outlook.com>
2021-08-31 11:23:41 +0200

    fds

--- progettAngular/src/app/app.component.ts
+++ progettAngular/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
+/*Com'è fatto un componente? 
+* Un compoenente ha un file html uno ts e uno css 
+* L'array di css interessa solo l'html del componente
+*/
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',                 //nome del tag html che usiamo per chiamare il componente 
+  templateUrl: './app.component.html',  //dove predere il codice html
+  styleUrls: ['./app.component.css']    //array per collegare i file .css
 })
+/* Tramite questa dichiarazione si creano le classi typeScript */
 export class AppComponent {
   title = 'progettAngular';
 }
