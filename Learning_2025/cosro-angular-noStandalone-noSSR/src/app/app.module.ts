8ee81aa912f843b9b12f4641aefd2bc1db5777bf
Andrea Dagg <daguannoandrea@outlook.com>
2025-02-04 16:19:26 +0100

    ngIF

---
+++ Learning_2025/cosro-angular-noStandalone-noSSR/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProvaComponent } from './prova/prova.component';
+
+// Angular Material 
+// Import the Angular Material modules after Angular's BrowserModule, as the import order matters for NgModules.
+// The Angular Material modules are imported after the BrowserModule to ensure that the Angular Material components are available for use in the application.
+// After importing the Angular Material modules, add them to the imports array in the @NgModule decorator. 
+// The imports array contains a list of external modules that the application needs.
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProvaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Learning_2025/cosro-angular-noStandalone-noSSR/src/app/prova/prova.component.ts
@@ -0,0 +1,83 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-prova',
+  templateUrl: './prova.component.html',
+  styleUrls: ['./prova.component.css'], // Corretto qui
+})
+// Implement the lifecycle hooks in the ProvaComponent class.
+// The lifecycle hooks are implemented as methods in the ProvaComponent class.
+// The methods are called by Angular at specific points in the component's lifecycle.
+// Anche se sono implementati in ordine diverso srispetto all'orine del lifecicle non è un problema
+// Dalla console del browser possiamo vedere l'esecuzione dei metodi del lifecycle
+export class ProvaComponent implements OnInit {
+  
+  isDisabled = false;
+  vaccinazione = true;
+
+  cani = [
+    {
+      nome: 'Roger',
+      razza: 'golden retriever',
+      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+        A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+        bred for hunting.`,
+        isVaccinato: true
+    },
+    {
+      nome: 'Rex',
+      razza: 'Pastore tedesco',
+      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+        A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+        bred for hunting.`,
+        isVavvinato: true
+    },
+    {
+      nome: 'Luna',
+      razza: 'Pastore Abbruzzese',
+      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+        A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+        bred for hunting.`,
+        isVaccinato: true
+    }
+  ];
+
+  constructor() {
+    console.log('Costruttore');
+  }
+
+  ngOnInit(): void {
+    console.log('print: ngOnInit');
+    setInterval(() => {
+      this.isDisabled = !this.isDisabled;
+      //console.log('isDisabled toggled:', this.isDisabled);
+    }, 2000);
+  }
+
+  //definiamo la funzione onClick che verrà chiamata quando clicchiamo sul bottone nel template
+  // Passa il componente come parametro
+  OnClick(e: any){
+    console.log('onClick');
+    console.log(e);
+    this.cani[0].nome = 'Fido';
+  }
+
+  OnInput(event: Event){
+    //console.log('onInput');
+    this.cani[0].nome = (<HTMLInputElement>event.target).value;
+  }
+
+  modifyVaccinazione(){
+    this.vaccinazione = !this.vaccinazione;
+  }
+
+}
