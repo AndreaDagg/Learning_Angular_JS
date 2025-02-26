46f5e4ac6008e0d9724e15399386363bea48ef88
Andrea D'Aguanno <daguannoandrea@outlook.com>
2025-02-26 09:36:47 +0100

    j

--- Learning_2025/TO-DO LIST AVANZATA CON ANGULAR/TO-DO-LIST-AVANZATA-CON-ANGULAR/src/app/filters/filters.component.ts
+++ Learning_2025/TO-DO LIST AVANZATA CON ANGULAR/TO-DO-LIST-AVANZATA-CON-ANGULAR/src/app/filters/filters.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import type { filterActive } from './filters.model';
+import { TodoitemsService } from '../shared/todoitems.service.ts.service';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
 export class FiltersComponent {
 
+
+
+  private filterActive: filterActive = {
+    filterActive: true,
+    filterCompleted: true,
+  };
+
+  constructor(private toDoService: TodoitemsService) {}
+
+  onFilter(type: 'active' | 'completed') {
+    if (type === 'active') {
+      this.filterActive.filterActive = !this.filterActive.filterActive;
+      //console.log( "Active: " + this.filterActive.filterActive + " Completed: " + this.filterActive.filterCompleted);
+    } else {
+      this.filterActive.filterCompleted = !this.filterActive.filterCompleted;
+      //console.log( "Active: " + this.filterActive.filterActive + " Completed: " + this.filterActive.filterCompleted);
+    }
+
+    this.toDoService.setFilterActive(this.filterActive);
+  }
+
+  get filters() {
+    return this.filterActive;
+  }
 }
--- Learning_2025/TO-DO LIST AVANZATA CON ANGULAR/TO-DO-LIST-AVANZATA-CON-ANGULAR/src/app/toolbar/toolbar.component.ts
+++ Learning_2025/TO-DO LIST AVANZATA CON ANGULAR/TO-DO-LIST-AVANZATA-CON-ANGULAR/src/app/toolbar/toolbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FiltersComponent } from "../filters/filters.component";
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [FiltersComponent],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
